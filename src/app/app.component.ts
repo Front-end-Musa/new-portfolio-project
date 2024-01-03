5a5b839c459c850967b18cd0b39f1d3a6b15d263
Musa Rufatuly <rufatulymusa567@gmail.com>
2024-01-03 16:15:52 +0600

    just backup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
+import { AboutComponent } from './components/about/about.component';
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { Routes } from '@angular/router';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ServicesComponent } from './components/services/services.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HeaderComponent,
+    MainPageComponent,
+    AboutComponent,
+    FooterComponent,
+    ProjectsComponent,
+    ServicesComponent,
+    BlogComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'new-portfolio-project';
+  title = 'project';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ServicesComponent } from './components/services/services.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: MainPageComponent },
+    { path: 'about-me', component: AboutComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'services', component: ServicesComponent },
+    { path: 'blog', component: BlogComponent },
+  ];
