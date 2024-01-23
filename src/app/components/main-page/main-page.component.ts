81c5de9707409eb3b0ec3f1919d736d48e736460
Musa Rufatuly <rufatulymusa567@gmail.com>
2024-01-23 22:50:24 +0600

    backup

--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './main-page.component.scss'
 })
 export class MainPageComponent {
-
+  scrollTo($element: HTMLElement): void {
+    $element.scrollIntoView({behavior: "smooth"})
+  }
 }
